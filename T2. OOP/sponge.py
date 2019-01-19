53c0ebf6e33b4ce39e03a38afb7a99b8a6325a97
Oleg Sedletskyi <frank.rtf@gmail.com>
2019-01-19 22:02:23 +0200

    Question regarding the solution to the practice.

--- T2. OOP/sponge.py
+++ T2. OOP/sponge.py
@@ -6,7 +6,8 @@ class Sponge():
         self.state = 'clean'
 
     def set_color(self, color):
-        print("Set sponge color")
+        print(f"Sponge set_color {self}")
+        print(f"Set sponge color {color}")
         self.color = color
 
 class Painter:
@@ -28,9 +29,14 @@ class SpongeFactory(Painter,Sponge):
         Painter().set_color(color)
         for sponge in range(self.sponge_count):
             sponge = Sponge()
-            sponge.set_color(color)
+            print(f"Created Sponge {sponge} with default color {sponge.color}")
+            super(Painter, self).set_color(color) # TODO:  Q: Не можу розібратись як ця стрічка коду (показана на занятті) може заміняти наступну (закоментовану). Додав прінти і бачу, що колір який ми додаємо у sponges не змінений, а лишається автозгенерованим (тобто цей рядок і наступний не є еквівалентними). Допоможіть, будь ласка, розібратись.
+            # sponge.set_color(color)
             # increase sponge factory produced count
+            self.__class__.produced_count += 1 # З цією все зрозуміло
             self.sponges.append(sponge)
+            print(f"SpongeFactory: sponge {sponge}, color {sponge.color}")
+            print("+" * 20)
 
 spf = SpongeFactory(10)
 spf.set_color(2)
