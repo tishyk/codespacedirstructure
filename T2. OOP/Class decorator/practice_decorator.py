c98c7869488195ea4fe12b5b885db330a6f75e29
tishyk <sytishchenko@gmail.com>
2019-08-28 10:10:56 +0300

    update decorator practice

--- T2. OOP/Class decorator/practice_decorator.py
+++ T2. OOP/Class decorator/practice_decorator.py
@@ -1,18 +1,19 @@
 # create function/class wrapper for the next methods
-# decorator print the name of decorated function
+# decorator print the name of decorated function inside decorator
 # object.__name__
+
 
 @debug
 def add(x, y):
     return x + y
 
 @debug
-def sub(x, y):
-    return x - y
+def sub(x, y, z):
+    return x - y, z
 
 @CWrapper
-def mul(x, y):
-    return x * y
+def mul(x, y, z):
+    return x * y, z
 
 @CWrapper
 def div(x, y):
