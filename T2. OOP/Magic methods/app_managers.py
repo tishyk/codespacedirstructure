563fadaa9c98f770ec84447e560cdae5edc70598
tishyk <sytishchenko@gmail.com>
2019-08-30 00:54:58 +0300

    add magic methods/descriptor example

---
+++ T2. OOP/Magic methods/app_managers.py
@@ -0,0 +1,41 @@
+# Application Managers
+
+
+from app_abstractions import ABCConnectionManager
+from connections import SSH
+
+class ConnectionManager(ABCConnectionManager):
+
+    def __new__(cls, *args, **kwargs):
+        if len(cls.get_network_interfaces())<1:
+            raise ConnectionError('Network interfaces not found!')
+        else:
+            return super().__new__(cls)
+
+    def create_ssh(self, ip, *args, **kwargs):
+        """Create ssh connection objects. Multiple sessions supported"""
+        ssh = SSH(ip, *args, **kwargs)
+        self.connections.setdefault(ssh, 'ssh connection')
+        return ssh
+
+    def create_ftp(self, *args, **kwargs):
+        # create ftp connection
+        pass
+
+    def create_serial(self, *args, **kwargs):
+        # create serial connection. Multiple session are not supported
+        pass
+
+    def create_telnet(self, *args, **kwargs):
+        # create telnet connection
+        pass
+
+    @classmethod
+    def get_network_interfaces(cls):
+        return ['wlan', 'mobile']
+
+if __name__ == "__main__":
+    connections = ConnectionManager()
+    ssh = connections.create_ssh('10.124.198.24')
+    for connection in connections.connections:
+        print(connection)
