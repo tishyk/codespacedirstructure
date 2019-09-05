19c05dd2490155cdf89fd70cb78db51b33391488
tishyk <sytishchenko@gmail.com>
2019-09-05 09:09:49 +0300

    changed to factory method pattern

--- T1. Design Patterns/Factory/factory_pizza.py
+++ T1. Design Patterns/Factory/factory_pizza.py
@@ -1,7 +1,16 @@
-class Pizza(object):
+import abc
+class ABCPizzaFactory(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def create_pizza(pizza_type):
+        """ Do something
+        # return Pizza object """
+        pass
+
+class Pizza(metaclass=abc.ABCMeta):
     def __init__(self):
         self._price = None
 
+    @abc.abstractmethod
     def get_price(self):
         return self._price
 
@@ -9,16 +18,24 @@ class HamAndMushroomPizza(Pizza):
     def __init__(self):
         self._price = 8.5
 
+    def get_price(self):
+        return self._price
+
 class DeluxePizza(Pizza):
     def __init__(self):
         self._price = 10.5
+
+    def get_price(self):
+        return self._price
 
 class HawaiianPizza(Pizza):
     def __init__(self):
         self._price = 11.5
 
-class PizzaFactory(object):
-    @staticmethod
+    def get_price(self):
+        return self._price
+
+class PizzaFactory(ABCPizzaFactory):
     def create_pizza(pizza_type):
         if pizza_type == 'HamMushroom':
             return HamAndMushroomPizza()
@@ -26,6 +43,8 @@ class PizzaFactory(object):
             return DeluxePizza()
         elif pizza_type == 'Hawaiian':
             return HawaiianPizza()
+        else:
+            raise AssertionError("Not implemented Pizza type")
 
 if __name__ == '__main__':
     for pizza_type in ('HamMushroom', 'Deluxe', 'Hawaiian'):
