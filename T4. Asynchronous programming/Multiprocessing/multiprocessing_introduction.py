1373f330a5728c582c1915a88fda8b8ba4bfe50b
tishyk <sytishchenko@gmail.com>
2019-09-26 21:50:11 +0300

    update multiprocessing intro

--- T4. Asynchronous programming/Multiprocessing/multiprocessing_introduction.py
+++ T4. Asynchronous programming/Multiprocessing/multiprocessing_introduction.py
@@ -8,21 +8,23 @@ def calc_square(numbers):
 
 def calc_cube(numbers):
     time.sleep(1)
-    for n in numbers:
-        print('cube ' + str(n*n*n))
+    while True:
+        for n in numbers:
+            print('cube ' + str(n*n*n))
 
 if __name__ == "__main__":
     arr = [2,3,8]
     p1 = multiprocessing.Process(target=calc_square, args=(arr,))
     p2 = multiprocessing.Process(target=calc_cube, args=(arr,))
+    #p2.daemon = True
 
     time.perf_counter()
     p1.start()
     p2.start()
 
-    p1.join()
-    p2.join()
-    print(p1.pid)
+    # p1.join()
+    #p2.join()
+    # print(p1.pid)
     p1.terminate()
     os.kill(p2.pid, signal.SIGTERM)
     print(p1.exitcode)
