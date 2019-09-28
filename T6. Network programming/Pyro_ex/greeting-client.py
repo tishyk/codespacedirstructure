a7218439b3dcf56e267f1f6aec7baf1f4babf2ce
tishyk <sytishchenko@gmail.com>
2019-09-29 00:15:09 +0300

    update Pyro4 examples to pyro5 with pyro.api

--- T6. Network programming/Pyro_ex/greeting-client.py
+++ T6. Network programming/Pyro_ex/greeting-client.py
@@ -1,7 +1,7 @@
 # saved as greeting-client.py
-import Pyro5
+from Pyro5.api import Proxy
 
 name = input("What is your name? ").strip()
 
-greeting_maker = Pyro5.Proxy("PYRONAME:example.greeting")    # use name server object lookup uri shortcut
+greeting_maker = Proxy("PYRONAME:example.greeting")    # use name server object lookup uri shortcut
 print(greeting_maker.get_fortune(name))
--- T6. Network programming/Pyro_ex/greeting-server.py
+++ T6. Network programming/Pyro_ex/greeting-server.py
@@ -1,14 +1,14 @@
 # saved as greeting-server.py
-import Pyro5
+from Pyro5.api import expose, Daemon, locate_ns
 
-@Pyro5.expose
+@expose
 class GreetingMaker(object):
     def get_fortune(self, name):
         return "Hello, {0}. Here is your fortune message:\n" \
                "Tomorrow's lucky number is 12345678.".format(name)
 
-daemon = Pyro5.Daemon()                # make a Pyro daemon
-ns = Pyro5.locateNS()                  # find the name server
+daemon = Daemon()                # make a Pyro daemon
+ns = locate_ns()                  # find the name server
 uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
 ns.register("example.greeting", uri)   # register the object with a name in the name server
 
