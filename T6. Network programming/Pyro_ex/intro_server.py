ff9ad8a060b29fb7b11131007d7cf60e052c4f0a
Tischenko Sergiy <sytischenko@gmail.com>
2019-09-28 23:53:31 +0300

    Update intro_server.py

--- T6. Network programming/Pyro_ex/intro_server.py
+++ T6. Network programming/Pyro_ex/intro_server.py
@@ -2,15 +2,15 @@
 #   Ready. Object uri = PYRO:obj_edb9e53007ce4713b371d0dc6a177955@localhost:51681
 
 # saved as greeting-server.py
-import Pyro4
+import Pyro5
 
-@Pyro4.expose
+@Pyro5.expose
 class GreetingMaker(object):
     def get_fortune(self, name):
         return "Hello, {0}. Here is your fortune message:\n" \
                "Behold the warranty -- the bold print giveth and the fine print taketh away.".format(name)
 
-daemon = Pyro4.Daemon()                # make a Pyro daemon
+daemon = Pyro5.Daemon()                # make a Pyro daemon
 uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
 
 print("Ready. Object uri =", uri)      # print the uri so we can use it in the client later
