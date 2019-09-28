3818b8244672c64082480f8cc1e8b26fdbf4e860
Tischenko Sergiy <sytischenko@gmail.com>
2019-09-28 23:52:06 +0300

    Update greeting-client.py

--- T6. Network programming/Pyro_ex/greeting-client.py
+++ T6. Network programming/Pyro_ex/greeting-client.py
@@ -1,7 +1,7 @@
 # saved as greeting-client.py
-import Pyro4
+import Pyro5
 
 name = input("What is your name? ").strip()
 
-greeting_maker = Pyro4.Proxy("PYRONAME:example.greeting")    # use name server object lookup uri shortcut
+greeting_maker = Pyro5.Proxy("PYRONAME:example.greeting")    # use name server object lookup uri shortcut
 print(greeting_maker.get_fortune(name))
