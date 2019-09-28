d6841f365cedf5fe72a635472eaa128b79bee465
Tischenko Sergiy <sytischenko@gmail.com>
2019-09-28 23:52:40 +0300

    Update greeting-server.py

--- T6. Network programming/Pyro_ex/greeting-server.py
+++ T6. Network programming/Pyro_ex/greeting-server.py
@@ -1,14 +1,14 @@
 # saved as greeting-server.py
-import Pyro4
+import Pyro5
 
-@Pyro4.expose
+@Pyro5.expose
 class GreetingMaker(object):
     def get_fortune(self, name):
         return "Hello, {0}. Here is your fortune message:\n" \
                "Tomorrow's lucky number is 12345678.".format(name)
 
-daemon = Pyro4.Daemon()                # make a Pyro daemon
-ns = Pyro4.locateNS()                  # find the name server
+daemon = Pyro5.Daemon()                # make a Pyro daemon
+ns = Pyro5.locateNS()                  # find the name server
 uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
 ns.register("example.greeting", uri)   # register the object with a name in the name server
 
