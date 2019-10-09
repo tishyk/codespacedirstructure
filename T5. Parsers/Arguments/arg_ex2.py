49947518b22b7f240a8c1d7df6883d721c589f2f
tishyk <sytishchenko@gmail.com>
2019-10-09 11:57:30 +0300

    xml example prettify

--- T5. Parsers/Arguments/arg_ex2.py
+++ T5. Parsers/Arguments/arg_ex2.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('agent_ip', type=str, default='127.0.0.0', help="Agent IP address to obtaining the file system")
-parser.add_argument('--ap', dest='password', type=str, help="Host 'root' password")
+parser.add_argument('-ap', dest='password', type=str, help="Host 'root' password")
 parser.add_argument('--master_ip', help=argparse.SUPPRESS)  # Argument transferred to ESD agent.py call
 
 mgrp = parser.add_mutually_exclusive_group()
