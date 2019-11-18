d61be94f67cc21c54d7c069ead898fa24691759b
tishyk <sytishchenko@gmail.com>
2019-11-18 22:55:08 +0200

    update unittest slides

--- T3. Code Testing/Unittest/employee.py
+++ T3. Code Testing/Unittest/employee.py
@@ -4,6 +4,7 @@ import requests
 class Employee:
     """A sample Employee class"""
     raise_amt = 1.05
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
