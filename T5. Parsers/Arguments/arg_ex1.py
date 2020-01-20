8d85499877a1d39ecb754dc72a0b257039b1b0cd
Tischenko Sergiy <sytischenko@gmail.com>
2020-01-20 18:54:11 +0200

    Update arg_ex1.py

--- T5. Parsers/Arguments/arg_ex1.py
+++ T5. Parsers/Arguments/arg_ex1.py
@@ -1,5 +1,19 @@
 import argparse
 
+"""
+name or flags - Either a name or a list of option strings, e.g. foo or -f, --foo.
+action - The basic type of action to be taken when this argument is encountered at the command line.
+nargs - The number of command-line arguments that should be consumed.
+const - A constant value required by some action and nargs selections.
+default - The value produced if the argument is absent from the command line.
+type - The type to which the command-line argument should be converted.
+choices - A container of the allowable values for the argument.
+required - Whether or not the command-line option may be omitted (optionals only).
+help - A brief description of what the argument does.
+metavar - A name for the argument in usage messages.
+dest - The name of the attribute to be added to the object returned by parse_args().
+"""
+
 parser = argparse.ArgumentParser()
 parser.add_argument('agent_ip', type=str, default='127.0.0.0', help="Agent IP address to obtaining the file system")
 parser.add_argument('agent_password',metavar='password', type=str, help="Agent host 'root' password")
